f4437e16634fcf3966284768f065a9ccf4541a71
marc-tan <marctan13@gmail.com>
2024-01-25 16:27:14 -0800

    cleaned code

---
+++ src/Button.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+
+export default function Button({ label }) {
+  return (
+      <button className="randomButton">
+        {label}
+      </button>
+  );
+}
